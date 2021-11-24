b4ba254aeeb546a5b055b4bed68fd7313da5c89e
Patrick <patrickdiehl@lsu.edu>
2021-11-23 20:30:54 -0600

    Push

---
+++ Docker/generate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo podman build --tag docker.io/diehlpk/monte-carlo-codes:latest -f ./Dockerfile
+sudo podman login docker.io
+id=$(sudo podman inspect --format="{{.Id}}" docker.io/diehlpk/monte-carlo-codes:latest)
+sudo podman push $id docker://diehlpk/monte-carlo-codes:latest
