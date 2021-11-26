9be2408a99346ece0bdb5c4393de78aab88a9de8
Patrick <patrickdiehl@lsu.edu>
2021-11-25 21:19:02 -0600

    Add swift

---
+++ swift/Sources/swift/main.swift
@@ -0,0 +1,40 @@
+
+// 
+// This file is part of the Monte carlo code exmaples.
+// Copyright (c) 2021 Patrick Diehl
+// 
+// This program is free software: you can redistribute it and/or modify  
+// it under the terms of the GNU General Public License as published by  
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License 
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+
+var nc = 0.0
+
+// Ask the user for the nummber of iterations
+print("Please enter the number of iterations:")
+let end = Int(readLine()!)!
+
+// Loop over the iterations
+for _ in 1...end {
+
+    let  x = Double.random(in: 0...1)
+    let y = Double.random(in: 0...1)
+    
+    if (x * x + y * y <= 1.0) {
+        nc += 1
+    }
+}
+
+// Compute the final result
+var pi = 4.0 * nc / Double(end)
+
+// Print the final result
+print(" Pi is equal to \(pi) after \(end) iterations")
