b01ec6d371fd7a41225beecb5c06cac9a7b20c81
Patrick Diehl <me@diehlpk.de>
2023-02-12 21:04:33 -0600

    Format`

--- swift/swift_heat/heat.swift
+++ swift/swift_heat/heat.swift
@@ -7,87 +7,82 @@ import Foundation
 
 let C_ARGV = CommandLine.arguments
 
-let nx = Int(C_ARGV[3]) ?? -1        // number of nodes
-let k = 0.5                         // heat transfer coefficient
-let dt = 1.0                        // time step
-let dx = 1.0                        // grid spacing
-let nt = Int(C_ARGV[2]) ?? -1        // number of time steps
+let nx = Int(C_ARGV[3]) ?? -1  // number of nodes
+let k = 0.5  // heat transfer coefficient
+let dt = 1.0  // time step
+let dx = 1.0  // grid spacing
+let nt = Int(C_ARGV[2]) ?? -1  // number of time steps
 let threads = Int(C_ARGV[1]) ?? -1  // numnber of threads
 
-func idx(_ i: Int,_  direction: Int) -> Int {
+func idx(_ i: Int, _ direction: Int) -> Int {
 
-    if (i == 0 && direction == -1)
-    { 
-        return nx - 1
-    }
-    if (i == nx - 1 && direction == +1)
-    {
-        return 0
-    }
-    assert ((i + direction) < nx)
+  if i == 0 && direction == -1 {
+    return nx - 1
+  }
+  if i == nx - 1 && direction == +1 {
+    return 0
+  }
+  assert((i + direction) < nx)
 
-    return i + direction
+  return i + direction
 
 }
 
-func heat(left: Double, middle: Double, right: Double) -> Double
-{
-    return middle + (k * dt / (dx * dx)) * (left - 2 * middle + right)
+func heat(left: Double, middle: Double, right: Double) -> Double {
+  return middle + (k * dt / (dx * dx)) * (left - 2 * middle + right)
 }
 
-func work(_ current: [Double], _ p: Int, _ threads: Int) async -> [Double] 
-{
-    let length = Int(nx / threads)
-    let start = p * length
-    var end = (p+1) * length - 1
-    var future = Array(repeating: 0.0 , count: length)
+func work(_ current: [Double], _ p: Int, _ threads: Int) async -> [Double] {
+  let length = Int(nx / threads)
+  let start = p * length
+  var end = (p + 1) * length - 1
+  var future = Array(repeating: 0.0, count: length)
 
-    if (p == threads-1 )
-    {    end = nx - 1 }
+  if p == threads - 1 { end = nx - 1 }
 
-    var index = 0
-    for i in start ... end
-    {   
-        future[index] = heat(left : current[idx(i,-1)], middle :
-            current[i], right :  current[idx(i,+1)])
-        index = index + 1
-    }
-    return future
+  var index = 0
+  for i in start...end {
+    future[index] = heat(
+      left: current[idx(i, -1)],
+      middle:
+        current[i], right: current[idx(i, +1)])
+    index = index + 1
+  }
+  return future
 }
 
-var space = [Array(repeating: 0.0,count: nx), Array(repeating: 0.0, count: nx)]
+var space = [Array(repeating: 0.0, count: nx), Array(repeating: 0.0, count: nx)]
 
-for i in 0 ... (nx-1) 
-{   
-    space[0][i] = Double(i)
+for i in 0...(nx - 1) {
+  space[0][i] = Double(i)
 }
 
-for t in 0 ... (nt-1)
-{
-    let current = space[t % 2]
-    var future = space[(t+1) % 2]
+for t in 0...(nt - 1) {
+  let current = space[t % 2]
+  var future = space[(t + 1) % 2]
 
-    await withTaskGroup(of : [Double].self, returning: Void.self, body: {
-    
-    group in
+  await withTaskGroup(
+    of: [Double].self, returning: Void.self,
+    body: {
 
-    for p in 0 ... (threads-1)
-    {
-    group.addTask { await  work(current,p,threads)   }
-    }
+      group in
 
-    future.removeAll()
+      for p in 0...(threads - 1) {
+        group.addTask { await work(current, p, threads) }
+      }
 
-    for await t in group {
+      future.removeAll()
+
+      for await t in group {
 
         for e in t {
-        future.append(e)
-}
+          future.append(e)
         }
+      }
 
     }
-    )
+  )
 
 }
 
-print(space[(nt-1)%2])
+print(space[(nt - 1) % 2])
