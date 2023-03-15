45d9f706133fe4b3d44e7ccaf69a259b109ef2f3
Steven R. Brandt <sbrandt@cct.lsu.edu>
2023-03-14 20:40:38 -0500

    Add other terms to fit performance loss

--- plotme.py
+++ plotme.py
@@ -18,7 +18,8 @@ with open('perfdata.csv','r',newline='') as cfd:
         if name not in xdata:
             xdata[name] = []
             ydata[name] = []
-        xdata[name] += [float(row[3])]
+        nthreads = float(row[3])
+        xdata[name] += [nthreads]
         ydata[name] += [float(row[6])]
 
 import matplotlib.pyplot as plt
@@ -31,21 +32,22 @@ for name in xdata:
     assert len(xv) == len(yv)
     plt.semilogy(xv,yv,'o',label=name)
 
-    def rt(x, k, p, o):
-        return k*(1-p + p/x + o*x)
+    def rt(x, k, p, o, o2, o3):
+        return k*(1-p + p/x + o*np.log(x) + o2*np.sqrt(x)+o3*x)
         
     if len(xv) > 3:
         try:
-            r = cf(rt,xv,yv,maxfev=5000,p0=(1,1,0),bounds=((0,0,0),(np.inf,1,1)))
-        except:
-            print("Could not fit curve for:",name)
+            r = cf(rt,xv,yv,maxfev=5000,p0=(1,1,0,0,0),bounds=((0,0,0,0,0),(np.inf,1,1,1,1)))
+        except Exception as e:
+            print("Could not fit curve for:",name,e)
             continue
         k=r[0][0]
         par=r[0][1]
         o=r[0][2]
         print("For ",name,": Parallel=","%.2g" % par," and Overhead=","%.2g" % o," using ",len(xv)," data pts.",sep='')
+        print(*r[0])
         xv2 = np.asarray(range(1,round(1+max(xdata[name]))))
-        yv2 = rt(xv2,k,par,o)
+        yv2 = rt(xv2,*r[0])
         plt.semilogy(xv2,yv2,'-',label='fit '+name)
 plt.legend()
 plt.show()
