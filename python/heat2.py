f4433f7602ada049212919bb0df1d72721b15181
Steven R. Brandt <sbrandt@cct.lsu.edu>
2023-03-22 09:59:21 -0500

    Make the code actually work correctly

--- python/heat2.py
+++ python/heat2.py
@@ -12,6 +12,8 @@ import time
 import os
 use_hw_counters : bool = sys.argv[4] == "1"
 
+check_correctness = False
+
 if use_hw_counters:
     from pypapi import events, papi_high as high
 
@@ -22,21 +24,24 @@ dx = 1.                      # grid spacing
 nt = int(sys.argv[2])        # number of time steps
 threads = int(sys.argv[1])   # numnber of threads
 
-tx = nx//threads
-assert tx > 0
-
 class Worker(Thread):
-    def __init__(self,num:int)->None:
+    def __init__(self,num:int,tx:int)->None:
         Thread.__init__(self)
         self.num : int = num
-        self.lo : int = tx*num
-        self.hi : int = tx*(num+1)
+        self.ghosts : int = 1
+        self.lo : int = tx*num-1
+        self.hi : int = tx*(num+1)+1
+        if self.lo < 0:
+            self.lo = 0
+        if self.hi > nx:
+            self.hi = nx
         self.right : Queue[float] = Queue()
         self.left : Queue[float] = Queue()
         self.sz = self.hi - self.lo
         assert self.sz > 0
         self.data = np.linspace(self.lo, self.hi-1, self.hi - self.lo)
         self.data2 = np.zeros((self.sz,))
+        assert self.data.shape == self.data2.shape
         self.leftThread  : 'Worker'
         self.rightThread : 'Worker'
 
@@ -53,8 +58,8 @@ class Worker(Thread):
         self.send_ghosts()
 
     def send_ghosts(self)->None:
-        self.leftThread.right.put_nowait(self.data[0])
-        self.rightThread.left.put_nowait(self.data[-1])
+        self.leftThread.right.put_nowait(self.data[1])
+        self.rightThread.left.put_nowait(self.data[-2])
 
     def run(self)->None:
         self.send_ghosts()
@@ -62,13 +67,15 @@ class Worker(Thread):
             self.update()
         self.recv_ghosts()
 
-def main()->Tuple[float,float]:
+def main(nthreads : int)->Tuple[float,float,np.ndarray]:
     th = []
-    for num in range(threads):
-        th += [Worker(num)]
-    for i in range(threads):
-        th[i].rightThread = th[(i+1) % threads]
-        th[(i+1)%threads].leftThread = th[i]
+    tx = nx//nthreads
+    assert tx > 0
+    for num in range(nthreads):
+        th += [Worker(num,tx)]
+    for i in range(nthreads):
+        th[i].rightThread = th[(i+1) % nthreads]
+        th[(i+1)%nthreads].leftThread = th[i]
 
     if use_hw_counters:
         high.start_counters([events.PAPI_FP_OPS,])
@@ -86,10 +93,20 @@ def main()->Tuple[float,float]:
     else:
         hw = 0
 
-    print(t2-t1)
-    return t2-t1, hw
+    total = np.zeros((nx,))
+    lo = 0
+    for t in th:
+        total[lo:lo+t.sz] = t.data2
+        lo += t.sz - 2*t.ghosts
 
-tdiff, hw = main()
+    print("time for ",nthreads,": ",t2-t1,sep="")
+    return t2-t1, hw, total
+
+if check_correctness:
+    tdiff, hw, data1 = main(1)
+tdiff, hw, dataN = main(threads)
+if check_correctness:
+    print("diff check:",np.max(np.abs(dataN - data1)))
 fn = 'perfdata.csv'
 if not os.path.exists(fn):
     with open(fn,"w") as fd:
