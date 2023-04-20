90f94325296871b34446ae9e3c43261803f852e8
Patrick Diehl <me@diehlpk.de>
2023-04-20 09:20:40 -0500

    New attempt for julia

--- julia/heat.jl
+++ julia/heat.jl
@@ -3,57 +3,24 @@
 #  SPDX-License-Identifier: BSL-1.0
 #  Distributed under the Boost Software License, Version 1.0. (See accompanying
 #  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-using DataStructures
+#using DataStructures
+using Distributed
 
-check_correctness = false
+@everywhere include("util.jl")
+
+workers = Threads.nthreads()
+
+addprocs(workers, 
+            restrict=true, 
+            enable_threaded_blas=true,
+            exeflags=`--optimize=3 --inline=yes --check-bounds=no --math-mode=fast`)
 
 nx = parse(Int64,ARGS[3])        # number of nodes
 k = 0.5                      # heat transfer coefficient
 dt = 1.                      # time step
 dx = 1.                      # grid spacing
 nt = parse(Int64,ARGS[2])        # number of time steps
-nthreads = parse(Int64,ARGS[1])    # numnber of threads   
-
-function idx(i, direction)
-
-    if i == 0 && direction == -1
-        return nx - 1
-    end
-
-    if i == nx - 1 && direction == +1
-        return 0
-
-    end
-
-    @assert ((i + direction) < nx)
-
-    return i + direction
-
-end
-
-function heat(left, middle, right)
-
-    return middle + (k * dt / (dx * dx)) * (left - 2 * middle + right)
-end
-
-function work(future, current, p,threads)
-    len = Int(round(nx / threads))
-    start = p * len + 1 
-    last = (p+1) * len 
-
-    if p == threads-1
-        last = nx 
-    end
-
-    n = length(current)
-    for i in range(start, last)
-        future[i] = heat(current[(i-1)%n+1],
-                         current[i], current[(i+1)%n+1])
-    end
-    
-end
-
-
+nthreads = parse(Int64,ARGS[1])    # numnber of threads  
 
 space = [zeros(nx), zeros(nx)]
 
@@ -67,19 +34,21 @@ for t in range(1, nt)
     current = space[t % 2+1]
     future = space[(t+1) % 2+1]
 
-    #tasks = []
+    tasks = []
   
-    #for i in 0:nthreads-1
-    #    push!(tasks,Threads.@spawn work(future,current,i,nthreads))
-    #end
-
-    Threads.@threads for i in 0:9
-        work(future,current,i,nthreads)    
+    for i in 0:nthreads-1
+        #push!(tasks,remotecall(work,i+1,future,current,i,nthreads))
+        push!(tasks,@spawnat i+1 work(future,current,i,nthreads))
     end
 
-    #for t in tasks
-    #    wait(t)
+    #Threads.@threads for i in 0:1
+     #   #println(i)
+     #   work(future,current,i,nthreads)    
     #end
+
+    for t in tasks
+        wait(t)
+    end
 end
 
 end
