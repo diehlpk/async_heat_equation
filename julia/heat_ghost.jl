0ecec645f1083dd94be4db84a2c4fd9492508c54
Steven R. Brandt <sbrandt@cct.lsu.edu>
2023-04-21 09:48:33 -0500

    Closer to working heat_ghost.jl

--- julia/heat_ghost.jl
+++ julia/heat_ghost.jl
@@ -3,7 +3,7 @@
 #  SPDX-License-Identifier: BSL-1.0
 #  Distributed under the Boost Software License, Version 1.0. (See accompanying
 #  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-using Distributed
+#using Distributed
 
 println(PROGRAM_FILE)
 
@@ -62,10 +62,12 @@ end
 
 #---- End implement the queues---"
 
+#@everywhere
 function work0(num)
     print("Start work0: ",num,"\n")
 end 
 
+#@everywhere
 function work(num)
     print("Start work: ",num,"\n")
     first = 1
@@ -95,32 +97,40 @@ function work(num)
     push_queue(leftq, ip1, data[first][sz-1])
     #print("Pre loop: ",num,"\n")
     for nt in range(1,nt)
-        print("nt: ",nt," ",data[first],"\n")
+        #print("nt: ",nt," ",data[first],"\n")
         data[first][1] = pop_queue(leftq, num)
         data[first][sz] = pop_queue(rightq, num)
 
         #print("Update: nt: ",nt," num: ",num,"\n")
         for i in range(2,sz-1)
+            #print("Data i: ",i,"\n")
             data[second][i] = data[first][i] + alp*(data[first][i+1] + data[first][i-1] - 2*data[first][i])
         end
+        #print("Post Update: nt: ",nt," num: ",num,"\n")
         push_queue(rightq, im1, data[first][2])
         push_queue(leftq, ip1, data[first][sz-1])
         # swap
         first = 3 - first
         second = 3 - second
     end
+    #print("End Evo\n")
     data[first][1] = pop_queue(leftq, num)
     data[first][sz] = pop_queue(rightq, num)
+    print("Finis!",data[first],"\n")
 end
 
 totalTime = @elapsed begin
-    tasks = []
-    for i in 0:nthreads-1
-        print("task: ",i+1)
-        push!(tasks,@spawnat i+1 work0(i))
-    end
-    for th in tasks
-        wait(th)
+    #tasks = []
+    #for i in 0:nthreads-1
+    #    print("task: ",i+1)
+    #    push!(tasks,@spawnat i+1 work(i))
+    #end
+    #for th in tasks
+    #    wait(th)
+    #end
+    Threads.@threads for i in 0:nthreads-1
+        #println(i)
+        work(i)
     end
 end
 print("total time: ",totalTime,"\n")
