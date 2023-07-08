36029f625d2d98fbc3f11907ee92dd8051ff8b53
Jamie Mair <JamieMair@users.noreply.github.com>
2023-07-08 09:00:36 +0100

    Move monte carlo pi calculation into a function
    This should be done for performance, as all global statements are interpreted. For compilation this needs to be inside a function. Also added the option to change the floating point precision. Could be further improved by using `@simd` macro on the for loop.

--- julia/monte/monte.jl
+++ julia/monte/monte.jl
@@ -3,26 +3,28 @@
 #  SPDX-License-Identifier: BSL-1.0
 #  Distributed under the Boost Software License, Version 1.0. (See accompanying
 #  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-#import RandomNumbers
 
-ncount = 0.0
 
 # Ask the user for the nummber of iterations
 print("Please enter the number of iterations:")
 n = parse(Int64,readline())
 
 #  Loop over the iterations
-for _ in 1:n
-    xVar = rand() * 2 -1
-    yVar = rand() * 2 -1
+function calculate_pi_serial(n, type=Float64)
+    ncount = zero(type)
+    for _ in 1:n
+        xVar = rand(type) * 2 - 1
+        yVar = rand(type) * 2 - 1
 
-    if xVar * xVar + yVar * yVar <= 1
-        global ncount += 1
+        if xVar * xVar + yVar * yVar <= 1
+            ncount += 1
+        end
     end
+    return 4 * ncount / n
 end
 
 # Compute the final result
-pi = 4.0 * ncount / n
+pi_est = calculate_pi_serial(n, Float64)
 
 # Print the final result
-print(" Pi is equal to $pi  after $n iterations")
+println(" Pi is equal to $pi_est  after $n iterations")
