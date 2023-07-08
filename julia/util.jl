b3506652b65e02c239970f0688a79be47bf16a3d
Jamie Mair <JamieMair@users.noreply.github.com>
2023-07-08 09:55:45 +0100

    Fixed indexing in the work function - is this meant to have periodic boundary conditions?

--- julia/util.jl
+++ julia/util.jl
@@ -4,18 +4,22 @@ function heat(left, middle, right, alpha)
 end
 
 function work(future, current, p, threads, nx, alpha)
-    len = cld(nx, nthreads)
+    len = cld(nx, threads)
     start = p * len + 1 
     last = min((p+1) * len, nx)
-
+    
     if p == threads-1
         last = nx 
     end
 
     n = length(current)
-    @inbounds @simd for i in range(start, last)
-        future[i] = heat(current[(i-1)%n+1],
-                         current[i], current[(i+1)%n+1],
+    @inbounds for i in start:last
+        left_index = (i-2+n) % n + 1
+        right_index = i % n + 1
+        future[i] = heat(current[left_index],
+                         current[i],
+                         current[right_index],
                          alpha)
     end
+    nothing
 end
