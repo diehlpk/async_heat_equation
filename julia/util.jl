4eab0b0e08a9ca749c3f17de80583e1a1c9feea8
Jamie Mair <JamieMair@users.noreply.github.com>
2023-07-08 09:16:59 +0100

    Made minor improvements to the work utils function
    Changed from `alp` to `alpha` and used the ceiling divide function instead of rounding. Remove bounds checks and added simd via macros.

--- julia/util.jl
+++ julia/util.jl
@@ -1,21 +1,21 @@
-function heat(left, middle, right, alp)
+function heat(left, middle, right, alpha)
     #return middle + (k * dt / (dx * dx)) * (left - 2 * middle + right)
-    return middle + alp * (left - 2 * middle + right)
+    return middle + alpha * (left - 2 * middle + right)
 end
 
-function work(future, current, p, threads, nx, alp)
-    len = Int(round(nx / threads))
+function work(future, current, p, threads, nx, alpha)
+    len = cld(nx, nthreads)
     start = p * len + 1 
-    last = (p+1) * len 
+    last = minimum((p+1) * len, nx)
 
     if p == threads-1
         last = nx 
     end
 
     n = length(current)
-    for i in range(start, last)
+    @inbounds @simd for i in range(start, last)
         future[i] = heat(current[(i-1)%n+1],
                          current[i], current[(i+1)%n+1],
-                         alp)
+                         alpha)
     end
 end
